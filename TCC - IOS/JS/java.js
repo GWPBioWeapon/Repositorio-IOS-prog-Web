4722be7943a6e7c80ebe1508fbc70931ed2e6e07
GWPBioWeapon <107894364+GWPBioWeapon@users.noreply.github.com>
2022-11-16 20:13:19 -0300

    Add files via upload

---
+++ TCC - IOS/JS/java.js
@@ -0,0 +1,4 @@
+function evento() {
+    var nome = document.agendamento.nome;
+    alert(`Ok, tudo certo ${nome}, suas informações foram enviadas e em breve \n entraremos em contato pelo email ou celular!`)
+}
