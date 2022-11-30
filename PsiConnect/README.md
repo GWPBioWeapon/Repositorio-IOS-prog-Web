f8c7057fd5ef1a7393b28d4f12b99bd7a2be02f8
GWPBioWeapon <107894364+GWPBioWeapon@users.noreply.github.com>
2022-11-30 11:07:15 -0300

    Delete PsiConnect directory

--- PsiConnect/README.md
+++
@@ -1,2 +0,0 @@
-# Repositorio-IOS-prog-Web
-Conteúdo das aulas IOS(HTML , CSS , Javascript, React e Git) de programação Web
--- PsiConnect/TCC - IOS/JS/java.js
+++
@@ -1,4 +0,0 @@
-function evento() {
-    var nome = document.agendamento.nome;
-    alert(`Ok, tudo certo ${nome}, suas informações foram enviadas e em breve \n entraremos em contato pelo email ou celular!`)
-}
